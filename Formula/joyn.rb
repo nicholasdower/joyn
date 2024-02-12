609e6a3bbff903977466dd43f1faeb754f5cfde0
Nick Dower <nicholasdower@gmail.com>
2024-02-12 11:44:45 +0100

    v1.0.0 Release
    - Join lines.

--- Formula/joyn.rb
+++ Formula/joyn.rb
@@ -2,7 +2,7 @@ class Joyn < Formula
   desc "Join lines"
   homepage "https://github.com/nicholasdower/joyn"
   url "https://github.com/nicholasdower/joyn/releases/download/v1.0.0/release.tar.gz"
-  sha256 "TBD"
+  sha256 "71a0f5123b5dd0afb6b3af461a16bce89207cd38753aec3815d4501c6c1cd8d5"
   license "MIT"
 
   def install
